ddd8979b129f4e5bd177fc8f3b737b116a2ffad5
YoshinoriIwaki <tanlab2fourth@gmail.com>
2018-01-11 05:11:51 +0900

    tdc study

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,7 +1,16 @@
 package money;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupyter.api.Test;
 
 public class MoneyTest {
-
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
+	}
 }
