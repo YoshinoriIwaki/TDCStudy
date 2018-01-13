8e0ee66ec01df62a9fac47310203f3c6af57706b
YoshinoriIwaki <tanlab@hotmail.co.jp>
2018-01-13 13:49:35 +0900

    tdc study

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,11 +1,10 @@
 package money;
 
 class Franc extends Money {
-	private int amount;
 	Franc(int amount){
 		this.amount = amount;
 	}
-	Franc times(int multiplier) {
+	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
 	}
 }
